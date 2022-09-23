19740b89057f802b588028e02aa624fbeaa24bee
Brendan Leal <leal.brendan@gmail.com>
2022-09-23 12:13:16 -0700

    extracte spotify authentication into it's own route

--- index.js
+++ index.js
@@ -1,87 +1,17 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-import axios from "axios";
+import "./src/config/loadEnv.js";
 import express from "express";
 import cors from "cors";
+import authRouter from "./src/routes/auth.js";
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
-
-const client_id = process.env.CLIENT_ID; // Your client id
-const client_secret = process.env.CLIENT_SECRET; // Your secret
+app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
   console.log("Test route");
-  console.log(req.query.code);
   res.json({ test: "Foo" });
-});
-
-function generateRandomString(length) {
-  var text = "";
-  var possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-}
-
-app.get("/auth/login", (req, res) => {
-  let scope = "streaming user-read-email user-read-private";
-
-  let state = generateRandomString(16);
-
-  let auth_query_parameters = new URLSearchParams({
-    response_type: "code",
-    client_id: client_id,
-    scope: scope,
-    redirect_uri: "http://localhost:3000/auth/callback",
-    state: state,
-  });
-
-  res.redirect(
-    "https://accounts.spotify.com/authorize/?" +
-      auth_query_parameters.toString()
-  );
-});
-
-app.get("/auth/callback", async (req, res) => {
-  console.log("In /auth/callback");
-  // console.log(req.query);
-  const queryParams = new URLSearchParams({
-    grant_type: "authorization_code",
-    code: req.query.code,
-    redirect_uri: "http://localhost:3000/auth/callback",
-  });
-  const auth = Buffer.from(
-    process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET
-  ).toString("base64");
-
-  console.log("auth: ", auth);
-  try {
-    const axiosRes = await axios({
-      method: "post",
-      url: "https://accounts.spotify.com/api/token",
-      params: queryParams,
-      // auth: { auth },
-      headers: { Authorization: "Basic " + auth },
-    });
-    // res.send(axiosRes);
-    console.log(axiosRes.data);
-    res.json(axiosRes.data);
-  } catch (e) {
-    console.log(e);
-    res.status(500).end();
-  }
-});
-
-app.get("/auth/token", (req, res) => {
-  res.json({
-    access_token: access_token,
-  });
 });
 
 app.listen(PORT, () => {
