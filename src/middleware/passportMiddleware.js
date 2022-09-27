8502cca7b4addc5417f8a0f17904e1aa7c0fd64a
Brendan Leal <leal.brendan@gmail.com>
2022-09-26 21:15:35 -0700

    upon successful auth save user and token info to DB

--- src/middleware/passportMiddleware.js
+++ src/middleware/passportMiddleware.js
@@ -1,3 +1,4 @@
+import client from "../db/client.js";
 import passport from "passport";
 import { Strategy as SpotifyStrategy } from "passport-spotify";
 
@@ -9,9 +10,12 @@ import { Strategy as SpotifyStrategy } from "passport-spotify";
 //   have a database of user records, the complete spotify profile is serialized
 //   and deserialized.
 passport.serializeUser(function (user, done) {
+  console.log(user);
   done(null, {
     displayName: user.displayName,
     isAuthed: true,
+    email: user.email,
+    isPremium: user.product === "premium", // FIXME: Do i need this in the session or should I be checking elsewhere?
   });
 });
 
@@ -19,6 +23,7 @@ passport.deserializeUser(function (obj, done) {
   done(null, obj);
 });
 
+// FIXME: refactor
 passport.use(
   new SpotifyStrategy(
     {
@@ -26,15 +31,81 @@ passport.use(
       clientSecret: process.env.CLIENT_SECRET,
       callbackURL: "http://localhost:" + process.env.PORT + "/auth/callback",
     },
-    function (accessToken, refreshToken, expires_in, profile, done) {
-      // asynchronous verification, for effect...
-      process.nextTick(function () {
-        // To keep the example simple, the user's spotify profile is returned to
-        // represent the logged-in user. In a typical application, you would want
-        // to associate the spotify account with a user record in your database,
-        // and return that user instead.
-        return done(null, profile);
-      });
+    async function (accessToken, refreshToken, expires_in, profile, done) {
+      const email = profile.emails[0].value;
+
+      const user = {
+        email,
+        userName: profile.username,
+        displayName: profile.displayName,
+        isPremium: profile.product === "premium",
+        profileUrl: profile.profileUrl,
+      };
+
+      try {
+        // Check if the user is already in the DB
+        const getUser = await client.query(
+          "SELECT email FROM users WHERE email=$1",
+          [email]
+        );
+        const userExists = getUser.rowCount === 1;
+
+        if (userExists) {
+          // TODO: user exists is their token still valid? if not need to refresh it
+          return done(null, user);
+        } else {
+          // add user to DB
+          const createUserQuery =
+            "INSERT INTO users (email, spotify_username, display_name, is_premium, profile_url) VALUES($1, $2, $3, $4, $5) RETURNING user_id";
+          const userValues = [
+            email,
+            profile.username,
+            profile.displayName,
+            profile.product === "premium",
+            profile.profileUrl,
+          ];
+          const createUser = await client.query(createUserQuery, userValues);
+          const userId = createUser.rows[0].user_id;
+
+          // Create timestamp of when access token was created.
+          const currentTimeInMilliSec = new Date().getTime();
+          const fiftyFiveMinutesInMilliSec = 60 * 60 * 1000 - 300000;
+          const timeToExpireInMilliSec =
+            currentTimeInMilliSec + fiftyFiveMinutesInMilliSec;
+
+          const expireTime = new Date(timeToExpireInMilliSec)
+            .toLocaleTimeString()
+            .replace(/[APM]/g, "")
+            .trim();
+          console.log(expireTime);
+
+          // save tokens in DB
+          const saveTokenQuery =
+            "INSERT INTO tokens (access_token, refresh_token, expire_time, user_id) VALUES($1, $2, $3, $4)";
+          const saveTokenValues = [
+            accessToken,
+            refreshToken,
+            expireTime,
+            userId,
+          ];
+          const saveTokens = await client.query(
+            saveTokenQuery,
+            saveTokenValues
+          );
+
+          console.log(createUser, saveTokens);
+          const createUserSuccess = createUser.rowCount === 1;
+          const saveTokenSuccess = saveTokens.rowCount === 1;
+
+          if (createUserSuccess && saveTokenSuccess) {
+            done(null, user);
+          } else {
+            throw new Error("something went wrong creating user");
+          }
+        }
+      } catch (err) {
+        console.log(err);
+      }
     }
   )
 );
