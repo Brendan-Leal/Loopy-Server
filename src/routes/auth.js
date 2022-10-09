182c6d1c9ec2ba4da247126f62d820d5ef1cf992
Brendan Leal <leal.brendan@gmail.com>
2022-10-09 14:48:27 -0700

    refactor to keep passport related configuration together

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,30 +1,22 @@
 import express from "express";
-import { passport as passportMiddleware } from "../middleware/passportMiddleware.js";
+import { redirectOnSuccess } from "../controllers/auth.js/index.js";
+import { passport, scope } from "../middleware/passport.js";
 
 const authRouter = express.Router();
-const scope = [
-  "streaming",
-  "user-modify-playback-state",
-  "user-read-email",
-  "user-read-private",
-  "user-read-playback-state",
-];
 
 authRouter.get(
   "/spotify",
-  passportMiddleware.authenticate("spotify", {
+  passport.authenticate("spotify", {
     scope,
   })
 );
 
 authRouter.get(
   "/callback",
-  passportMiddleware.authenticate("spotify", {
-    failureRedirect: "http://localhost:3000/",
+  passport.authenticate("spotify", {
+    failureRedirect: "http://localhost:3000/fail",
   }),
-  function (req, res) {
-    res.redirect("http://localhost:3000/login-success");
-  }
+  redirectOnSuccess
 );
 
 export default authRouter;
