95afd9750a00b4693ea444d818725e249b954ce5
Brendan Leal <leal.brendan@gmail.com>
2022-10-09 14:46:09 -0700

    schema change

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,6 +1,9 @@
+DROP TABLE IF EXISTS tokens;
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users (
     user_id serial PRIMARY KEY,
-    email text,
+    email text UNIQUE, 
     spotify_username text,
     display_name text,
     is_premium boolean,
@@ -12,5 +15,8 @@ CREATE TABLE tokens (
     user_id integer REFERENCES users(user_id),
     access_token text,
     refresh_token text,
-    expire_time TIMESTAMP 
+    expire_time TIMESTAMPTZ 
 );
+
+INSERT INTO users (email, spotify_username, display_name, is_premium, profile_url)
+VALUES ('baz@foo.com', 'baz', 'bazzy', true, 'https://open.spotify/user/baz');
